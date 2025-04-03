6ef7fad699ad1f648a2a2e069738d04d69b595af
Gaita <perminusgaita1@gmail.com>
2025-04-03 13:21:49 +0300

    removed cron job

--- app/api/cron/daily/route.js
+++ app/api/cron/daily/route.js
@@ -1,17 +1,17 @@
-export const dynamic = 'force-dynamic'
-import { NextResponse } from "next/server";
-import { deleteS3ObjectsTaggedForDeletion } from "@/services/media/delete/delete-s3-objects-tagged-for-deletion"
+// export const dynamic = 'force-dynamic'
+// import { NextResponse } from "next/server";
+// import { deleteS3ObjectsTaggedForDeletion } from "@/services/media/delete/delete-s3-objects-tagged-for-deletion"
 
-export async function GET(request) {
-  // check if there are any s3 objects to be deleted.
-  // if you put a security sensitive operation here you MUST use authentication
+// export async function GET(request) {
+//   // check if there are any s3 objects to be deleted.
+//   // if you put a security sensitive operation here you MUST use authentication
   
-  // delete expired s3 objects
-  await deleteS3ObjectsTaggedForDeletion();
-  return NextResponse.json(
-    { message: "Success" },
-    { status: 200 }
-  );
-}
+//   // delete expired s3 objects
+//   await deleteS3ObjectsTaggedForDeletion();
+//   return NextResponse.json(
+//     { message: "Success" },
+//     { status: 200 }
+//   );
+// }
 
 
